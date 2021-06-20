a2669a5478ea72c7d888b13613b728cdd135ddf6
Saman Hosseini <bounoable@gmail.com>
2021-06-20 20:54:32 +0200

    chore(media): define `DocumentService`

--- media/media.go
+++ media/media.go
@@ -24,16 +24,10 @@ type ImageService interface {
 
 	// Replace replaces the image at the specified disk and path with the image
 	// in Reader and returns the updated Image.
-	//
-	// Replace returns ErrUnknownImage if the image does not exist in the
-	// ImageRepository or ErrUploadFailed if the upload of the new image fails.
 	Replace(_ context.Context, _ io.Reader, disk, path string) (Image, error)
 
 	// Rename renames the image at the specified disk and path to the given name
-	// and returns the renamed Image.
-	//
-	// Rename returns ErrUnknownImage if the image does not exist in the
-	// ImageRepository.
+	// and returns the updated Image.
 	Rename(_ context.Context, name, disk, path string) (Image, error)
 
 	// Delete deletes the image at the specified disk and path. Delete returns
@@ -41,12 +35,48 @@ type ImageService interface {
 	Delete(_ context.Context, disk, path string) error
 
 	// Tag tags the specified image with the given tags and returns the updated
-	// Image. Tag returns ErrUnknownImage if the image does not exist.
+	// Image.
 	Tag(_ context.Context, disk, path string, tags ...string) (Image, error)
 
 	// Untag removes the given tags from the specified image and returns the
-	// updated Image. Untag returns ErrUnknownImage if the image does not exist.
+	// updated Image.
 	Untag(_ context.Context, disk, path string, tags ...string) (Image, error)
+}
+
+// DocumentService provides upload and management of Documents.
+type DocumentService interface {
+	// Upload uploads the document in Reader to the given storage disk and path
+	// with the provided documentName and filename and returns the uploaded
+	// Document.
+	//
+	// If the upload fails, ErrUploadFailed is returned.
+	Upload(_ context.Context, documentName, filename, disk, path string, filesize int) (Document, error)
+
+	// Download downloads the Document from the specified disk and path.
+	//
+	// Download returns ErrFileNotFound if the file does not exist in the
+	// storage.
+	Download(context.Context, Document) ([]byte, string, error)
+
+	// Replace replaces the document at the specified disk and path with the
+	// document in Reader and returns the updated Document.
+	Replace(_ context.Context, _ io.Reader, disk, path string) (Image, error)
+
+	// Rename renames the document at the specified disk and path to the given name
+	// and returns the updated Document.
+	Rename(_ context.Context, name, disk, path string) (Image, error)
+
+	// Delete deletes the document at the specified disk and path. Delete returns
+	// ErrFileNotFound if the document does not exist.
+	Delete(context.Context, Document) error
+
+	// Tag tags the specified document with the given tags and returns the
+	// updated Document.
+	Tag(_ context.Context, disk, path string, tags ...string) (Document, error)
+
+	// Untag removes the given tags from the specified document and returns the
+	// updated Document.
+	Untag(_ context.Context, disk, path string, tags ...string) (Document, error)
 }
 
 // ImageEncoder encodes images, using the appropriate encoder for the specified
@@ -135,7 +165,7 @@ func (f File) Same(other File) bool {
 	return f.Disk == other.Disk && f.Path == other.Path
 }
 
-// Image is an uploaded image.
+// Image is storage image.
 type Image struct {
 	File
 
@@ -149,5 +179,22 @@ func NewImage(width, height int, name, disk, path string, filesize int) Image {
 		File:   NewFile(name, disk, path, filesize),
 		Width:  width,
 		Height: height,
+	}
+}
+
+// Document is an arbitrary storage file.
+type Document struct {
+	File
+
+	// DocumentName is the unique name of the document. DocumentName may be
+	// empty but if it is not, it must be unique.
+	DocumentName string
+}
+
+// NewDocument returns a Document with the given data.
+func NewDocument(documentName, name, disk, path string, filesize int) Document {
+	return Document{
+		File:         NewFile(name, disk, path, filesize),
+		DocumentName: documentName,
 	}
 }
