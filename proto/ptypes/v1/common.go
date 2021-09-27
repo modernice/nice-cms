1f7f96c8c7272d32cea9cc1149204959c7274652
Saman Hosseini <bounoable@gmail.com>
2021-09-27 20:16:28 +0200

    refactor(proto): export proto definitions

---
+++ proto/ptypes/v1/common.go
@@ -0,0 +1,18 @@
+package ptypes
+
+import (
+	"github.com/google/uuid"
+	protocommon "github.com/modernice/nice-cms/proto/gen/common/v1"
+)
+
+// UUIDProto encodes a UUID.
+func UUIDProto(id uuid.UUID) *protocommon.UUID {
+	return &protocommon.UUID{Bytes: id[:]}
+}
+
+// UUID deoodes a UUID.
+func UUID(id *protocommon.UUID) uuid.UUID {
+	var b [16]byte
+	copy(b[:], id.GetBytes())
+	return uuid.UUID(b)
+}
